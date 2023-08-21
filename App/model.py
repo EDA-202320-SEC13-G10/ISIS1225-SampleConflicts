c1ef845a65f0d0cce05daee16afdb4935b4536f5
mmartinezu204 <mauri.martinezurbaneja@gmail.com>
2023-08-21 16:30:06 -0500

    Cambios Est-3 model.py

--- App/model.py
+++ App/model.py
@@ -75,8 +75,9 @@ def addBookTags(catalog, booktagsfile):
     Esta funcion guardar los booktags provenientes del archivo CSV.
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
-
+    catalog["book_tags"] = lt.newList(datastructure="ARRAY_LIST",
+                                      filename=booktagsfile)
+    return catalog
 
 def addTag(catalog, tag):
     """
@@ -102,7 +103,8 @@ def createBookTagList(catalog):
     Esta funcion crea una lista vacia para booktags.
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
+    catalog["book_tags"] = lt.newList(datastructure="ARRAY_LIST")
+    return catalog
 
 
 def addBookTag(catalog, booktag):
@@ -110,31 +112,31 @@ def addBookTag(catalog, booktag):
     Esta funcion agrega un elemento a lista de booktags.
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
-
+    lt.addLast(catalog["book_tags"], booktag)
+    return catalog
 
 # Funciones de consulta
 
 def bookSize(catalog):
     # TODO Mods de Est-3 en el Lab 2
-    pass
+    return lt.size(catalog["books"])
 
 
 def tagSize(catalog):
     # TODO Mods de Est-3 en el Lab 2
-    pass
+    return lt.size(catalog["tags"])
 
 
 def bookTagSize(catalog):
     # TODO Mods de Est-3 en el Lab 2
-    pass
+    return lt.size(catalog["book_tags"])
 
 
 def firstBook(catalog):
     # TODO Mods de Est-3 en el Lab 2
-    pass
+    return lt.firstElement(catalog["books"])
 
 
 def lastBook(catalog):
     # TODO Mods de Est-3 en el Lab 2
-    pass
+    return lt.lastElement(catalog["books"])
