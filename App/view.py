9fbcea2b5e854f8ae8aea03937897981f865172f
JuanDavid-21 <jd.ortizp1@uniandes.edu.co>
2023-08-21 18:59:46 -0500

    cambios est2

--- App/view.py
+++ App/view.py
@@ -93,7 +93,9 @@ def lastBook(control):
     """
     Devuelve el último libro cargado
     """
-    pass
+    last=controller.lastBook(control)
+    return last
+
 
 
 # Se crea el controlador asociado a la vista
